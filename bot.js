52ac288cc5eafc0b7412a7bf61e5bbe198600f21
Abdus <mail@abdus.xyz>
2020-01-13 20:58:28 +0530

    removed duplicate tweets from database, enabled stream api

--- bot.js
+++ bot.js
@@ -1,48 +1,27 @@
-require("dotenv").config();
-const twit = require("twit");
-const config = require("./config.js");
+require('dotenv').config();
+const twit = require('twit');
+const config = require('./config.js');
 const client = new twit(config);
-const CronJob = require("cron").CronJob;
-require("./web/server")(client);
+const CronJob = require('cron').CronJob;
+require('./web/server')(client);
 
-const TweetsService = require("./database/services/tweets.service");
+const TweetsService = require('./database/services/tweets.service');
 
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 mongoose
-    .connect(process.env.MONGO_DB, {
-        useNewUrlParser: true,
-        useFindAndModify: false,
-        useUnifiedTopology: true
-    })
-    .then(() => console.log("DATABASE CONNECTION ESTABLISHED"));
+  .connect(process.env.MONGO_DB, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DATABASE CONNECTION ESTABLISHED'));
 
-console.log("BOT IS RUNNING");
+console.log('BOT IS RUNNING');
 
-new CronJob(
-    "0 */10 * * * *",
-    function() {
-        let params = {
-            q: "#30DayProject",
-            result_type: "recent",
-            count: 100
-        };
-
-        async function gotTweets(err, data, response) {
-            if (!err) {
-                await data.statuses.map(tweet => {
-                    TweetsService.saveTweet(tweet);
-                });
-                if (data.statuses.length === 100) {
-                    params.max_id = data.statuses[99].id;
-                    client.get("search/tweets", params, gotTweets);
-                }
-            } else console.error(`Something went wrong: `, err);
-        }
-
-        client.get("search/tweets", params, gotTweets);
-        console.log("Updating Tweets");
-    },
-    null,
-    true,
-    "America/Los_Angeles"
-);
+const stream = client.stream('statuses/filter', { track: '#30DayProject' });
+stream.on('tweet', tweet => {
+  if (!tweet.text.startsWith('RT @')) {
+    TweetsService.saveTweet(tweet);
+    console.log('Updating Tweets');
+  } else console.log('Tweet is a RT');
+});
